84c441a99df0bf3d8f22afd2c876fb00dea35a10
kennethrfrank <kennethrfrankjr@gmail.com>
2020-05-26 21:03:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
 
+		//Animate Contact
+
+	function emailSlide(){
+		$("#cUs").slideUp();
+		$("#email").slideDown();
+
+		setTimeout(
+			function(){
+				$("#email").slideUp();
+				$("#cUs").slideDown();
+			}
+			, 10000);
+	}
+
+
 	$("img#logo").on("click", function(){
 		console.log("hello friend");
 	});
 
-	$("#cUs.cUs").on("mouseover", function(){
-		console.log("hover");
-		$("#cUs").slideUp();
-		$("#email").slideDown();
+	$("#cUs.cUs").on("click", function(){
+		window.open('mailto:contactnclyne@gmail.com?subject=WebInquiry&body=body');
+		emailSlide();
 
 	});
+
+	// $("#cUs.cUs").on("mouseover", function(){
+	// 	console.log("hover");
+	// 	$("#cUs").slideUp();
+	// 	
+
+	// });
 
 	
 
